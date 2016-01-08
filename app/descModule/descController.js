8a49fea8121d469630781a83427cd8dca148740e
Edouard Strauser <edouard.strauser@vidal.fr>
2016-01-08 11:37:02 +0100

    working v1

--- app/descModule/descController.js
+++ app/descModule/descController.js
@@ -2,39 +2,83 @@ angular.module('desc-controller', ['desc-service'])
     .controller('descController', function($scope, descService) {
 
     	$scope.accessories = descService.getAccessories();
+        $scope.newItems = [];
 
-		var editor = ace.edit("editor")
-		editor.setTheme("ace/theme/monokai");
-        editor.getSession().setMode("ace/mode/javascript");
+        for(var categoryI in $scope.accessories) {
+            $scope.newItems.push({});
+        }
 
-        editor.renderer.setShowGutter(false);
-        editor.setHighlightActiveLine(false);
-        editor.setReadOnly(true);
 
+            var introEditor = initEditor("introEditor", true, descService.getIntro());
+        var outroEditor = initEditor("outroEditor", true, descService.getOutro());
+        var resultEditor = initEditor("resultEditor", false, computeResult());
+
+        introEditor.on('change', function() { resultEditor.setValue(computeResult(), 1); });
+        outroEditor.on('change', function() { resultEditor.setValue(computeResult(), 1); });
+
+
+        $scope.selectCheckBox = function(item) {
+            resultEditor.setValue(computeResult(), 1);
+        };
         $scope.selectItem = function(item) {
             item.selected = !item.selected;
-            computeResult();
+            resultEditor.setValue(computeResult(), 1);
+        };
+
+        $scope.newItem = function(index) {
+            var name = $scope.newItems[index].name;
+            var desc = $scope.newItems[index].desc;
+            if(name) {
+                $scope.accessories[index].list.push({name: name, desc: desc});
+                $scope.newItems[index].name = "";
+                $scope.newItems[index].desc = "";
+            }
         };
 
         function computeResult() {
             result = "";
 
+            result += introEditor.getValue() + "\n";
+
             for(var categoryI in $scope.accessories) {
                 var category = $scope.accessories[categoryI];
 
-
-                result += category.name + "\n\n";
-
                 for(var itemI in category.list) {
                     var item = category.list[itemI];
-                    if(item.selected) {
-                        result += item.name + " " + item.desc + "\n";
+                    if (item.selected) {
+                        result += category.name + "\n" + getSelectedItemList(category.list) + "\n";
+                        break;
                     }
                 }
-
-                result += "\n";
             }
 
-            editor.setValue(result, 1);
+            result += outroEditor.getValue();
+
+            return result;
+        }
+
+        function getSelectedItemList(list) {
+            var result = "";
+
+            for(var itemI in list) {
+                var item = list[itemI];
+                if(item.selected) {
+                    result += item.name + " " + item.desc + "\n";
+                }
+            }
+
+            return result;
+        }
+
+        function initEditor(editorName, modifiable, initContent) {
+            var editor = ace.edit(editorName);
+            editor.setTheme("ace/theme/monokai");
+
+            editor.renderer.setShowGutter(false);
+            editor.setHighlightActiveLine(false);
+            editor.setReadOnly(!modifiable);
+            editor.setValue(initContent, 1);
+
+            return editor;
         }
 });
--- app/descModule/list.js
+++ app/descModule/list.js
@@ -4,8 +4,12 @@ angular.module('desc-service', [])
     .factory('descService', function ($http) {
         var service = {};
 
+        service.getIntro = function() {
+            return "Voici 5 idées de nail art pour Noël ♡\n" +
+                    "↓↓ Plus d'infos ↓↓\n";
+        };
+
         service.getAccessories = function(){
-          
             return [
                 {
                     name: "Vernis",
@@ -38,7 +42,25 @@ angular.module('desc-service', [])
                     ]
                 }
             ]
+        };
 
+        service.getOutro = function() {
+            return "En Intro :\n" +
+                "♡ Ce que je porte :\n" +
+                "- \n\n" +
+
+                "Si vous re-créez un de mes nail art, n'hésitez pas à me taguer sur la photo @yokonailart et avec le hashtag #yokonailart sur Instagram pour que j'aille liker votre photo ♡♡♡\n\n" +
+
+                "A B O N N E Z  V O U S : http://www.youtube.com/subscription_center?add_user=1fashionail\n" +
+                "S H O P : http://www.bijoux-faits-main.com\n" +
+                "F A C E B O O K : https://www.facebook.com/pages/Yoko-Nail-Art/101032073271306\n" +
+                "I N S T A G R A M : @yokonailart\n" +
+                "T W I T T E R : @yokonailart\n" +
+                "*Merci à Beauté Privée d’avoir sponsorisé ce contenu\n\n" +
+
+                "Je filme avec un Nikon D5100 et je monte avec Final Cut Pro X\n" +
+                "Musique Youtube Audio Library\n" +
+                "(Source : http://incompetech.com/music/royalty-free/index.html?isrc=USUAN1400037)\n";
         };
 
         return service;
